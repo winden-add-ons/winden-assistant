681e6c4ebcd29ebb30565d9ea85913861dddf307
Marko Krstic <mailme@markokrstic.com>
2023-12-16 19:46:08 +0100

    ...

--- inc/TailwindConfigLoader.php
+++ inc/TailwindConfigLoader.php
@@ -7,7 +7,24 @@ class TailwindConfigLoader
     public function getTwConfigFromFile()
     {
         $configPath = plugin_dir_path(__FILE__) . '../data/tailwind.config.js';
-        $configContent = file_get_contents($configPath);
+
+        // Check if the file exists
+        if (file_exists($configPath)) {
+            $configContent = file_get_contents($configPath);
+        } else {
+            // File does not exist, load content from database
+            $option_name = 'winden_editor';
+            $option_value = get_option($option_name);
+
+            if (is_array($option_value) && count($option_value) > 0) {
+                $configContent = isset($option_value[1]['content']) ? $option_value[1]['content'] : 'No content available';
+            } else {
+                echo 'No config content available in the database.';
+                return;
+            }
+        }
+
+        // Continue processing the config content
         $screens = TailwindConfigParser::parse($configContent);
 
         if (empty($screens)) {
