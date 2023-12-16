6a6b44b4460adfd9bc446a4fbc095e4028092c7e
Marko Krstic <mailme@markokrstic.com>
2023-12-16 20:17:36 +0100

    Update winden-assistant.php

--- winden-assistant.php
+++ winden-assistant.php
@@ -31,6 +31,60 @@ function parse_tailwind_config($content)
   return $screens;
 }
 
+
+
+// get tw config from file
+add_action('wp_footer', 'get_tw_config_from_file');
+add_action('admin_footer', 'get_tw_config_from_file');
+
+function get_tw_config_from_file()
+{
+  $configPath = plugin_dir_path(__FILE__) . 'data/tailwind.config.js';
+
+  // Check if the file exists
+  if (file_exists($configPath)) {
+    $configContent = file_get_contents($configPath);
+  } else {
+    // File does not exist, load content from database
+    $option_name = 'winden_editor';
+    $option_value = get_option($option_name);
+
+    if (is_array($option_value) && count($option_value) > 0) {
+      $configContent = isset($option_value[1]['content']) ? $option_value[1]['content'] : 'No content available';
+    } else {
+      echo 'No config content available in the database.';
+      return;
+    }
+  }
+
+  // Parse the Tailwind config file.
+  // Note: You might need to write a custom parser or use an existing library, as this file is in JavaScript format.
+  $screens = parse_tailwind_config($configContent);
+
+  if (empty($screens)) {
+    echo 'No screen values found or unable to parse the config file.';
+    return;
+  }
+
+  $jsContent = '<script>';
+  $jsContent .= 'var tailwindScreens = [];'; // Define a JavaScript array variable.
+
+  foreach ($screens as $name => $size) {
+    // Append the data to the JavaScript variable.
+    $jsContent .= "tailwindScreens.push({ name: '{$name}', size: '{$size}' });";
+  }
+
+  $jsContent .= 'console.log("get_tw_config_from_file loaded");';
+  $jsContent .= '</script>';
+
+  // Output the JavaScript variable content.
+  echo $jsContent;
+}
+
+
+
+
+
 function winden_assistant_enqueue_script()
 {
   // Check if the current user is an administrator
@@ -64,40 +118,6 @@ function winden_assistant_enqueue_script()
 
 add_action('wp_enqueue_scripts', 'winden_assistant_enqueue_script');
 
-
-
-
-
-// get tw config from file
-add_action('wp_footer', 'get_tw_config_from_file');
-
-function get_tw_config_from_file()
-{
-  $configPath = plugin_dir_path(__FILE__) . 'data/tailwind.config.js';
-  $configContent = file_get_contents($configPath);
-
-  // Parse the Tailwind config file.
-  // Note: You might need to write a custom parser or use an existing library, as this file is in JavaScript format.
-  $screens = parse_tailwind_config($configContent);
-
-  if (empty($screens)) {
-    echo 'No screen values found or unable to parse the config file.';
-    return;
-  }
-
-  $jsContent = '<script>';
-  $jsContent .= 'var tailwindScreens = [];'; // Define a JavaScript array variable.
-
-  foreach ($screens as $name => $size) {
-    // Append the data to the JavaScript variable.
-    $jsContent .= "tailwindScreens.push({ name: '{$name}', size: '{$size}' });";
-  }
-
-  $jsContent .= '</script>';
-
-  // Output the JavaScript variable content.
-  echo $jsContent;
-}
 
 
 // Global variable to store the page hook
