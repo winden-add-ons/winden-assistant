db2c39e1b47e7258f98c6a2e9f0265edffb1f422
Marko Krstic <mailme@markokrstic.com>
2023-12-21 15:06:12 +0100

    Update TailwindConfigParser.php

--- inc/TailwindConfigParser.php
+++ inc/TailwindConfigParser.php
@@ -4,9 +4,35 @@ class TailwindConfigParser
 {
     public static function parse($content)
     {
-        $screens = [];
-        $pattern = '/screens:\s*\{\s*(.*?)\s*\}(,|})/s';
+        // Default breakpoints
+        $defaultScreens = [
+            'sm' => '640px',
+            'md' => '768px',
+            'lg' => '1024px',
+            'xl' => '1280px',
+            '2xl' => '1536px',
+        ];
 
+        // Parsing pattern for direct screens
+        $directPattern = '/theme:\s*\{\s*screens:\s*\{\s*(.*?)\s*\}\s*(,|})/s';
+        $directScreens = self::parseScreens($content, $directPattern);
+
+        // If direct screens are found, use them instead of default screens
+        if (!empty($directScreens)) {
+            return $directScreens;
+        }
+
+        // Parsing pattern for extended screens
+        $extendPattern = '/extend:\s*\{\s*screens:\s*\{\s*(.*?)\s*\}\s*(,|})/s';
+        $extendedScreens = self::parseScreens($content, $extendPattern);
+
+        // Combine default screens with extended screens, if extended screens are provided
+        return array_merge($defaultScreens, $extendedScreens);
+    }
+
+    private static function parseScreens($content, $pattern)
+    {
+        $screens = [];
         if (preg_match_all($pattern, $content, $matches, PREG_SET_ORDER)) {
             foreach ($matches as $match) {
                 $screenPattern = '/(\w+):\s*"([^\"]+)"/';
@@ -17,18 +43,6 @@ class TailwindConfigParser
                 }
             }
         }
-
-        // Check if no breakpoints are found and add default breakpoints
-        if (empty($screens)) {
-            $screens = [
-                'sm' => '640px',
-                'md' => '768px',
-                'lg' => '1024px',
-                'xl' => '1280px',
-                '2xl' => '1536px',
-            ];
-        }
-
         return $screens;
     }
 }
