c8807eda32f028d7e9161ece51c104c3174bb446
Marko Krstic <mailme@markokrstic.com>
2023-12-29 20:28:19 +0100

    ...

--- inc/tailwindScreens.php
+++ inc/tailwindScreens.php
@@ -55,4 +55,4 @@ class TailwindScreensInjector
     }
 }
 
-new TailwindScreensInjector();
+
--- winden-assistant.php
+++ winden-assistant.php
@@ -11,6 +11,7 @@
 
 require_once 'inc/AdminMenu.php';
 require_once 'inc/TailwindConfigLoader.php'; // Ensure this file is included
+require_once 'inc/tailwindScreens.php'; // Ensure this file is included
 
 class WindenAssistant
 {
@@ -22,13 +23,13 @@ class WindenAssistant
     $this->adminMenu = new AdminMenu();
     $this->tailwindConfigLoader = new TailwindConfigLoader();
 
-    //add_action('wp_enqueue_scripts', array($this->scriptLoader, 'enqueueScript'));
     add_action('wp_footer', array($this->tailwindConfigLoader, 'getTwConfigFromFile'));
     add_action('admin_footer', array($this->tailwindConfigLoader, 'getTwConfigFromFile'));
     add_action('admin_menu', array($this->adminMenu, 'addAdminMenu'));
   }
 }
 
+new TailwindScreensInjector();
 // Initialize the plugin
 new WindenAssistant();
 
